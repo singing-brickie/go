9d1d7f25877348ab07bb2f156568cb9d21c0c330
coding-monk <rburnsme1@gmail.com>
2017-08-07 21:11:15 +0100

    added login page

--- server-side/test.go
+++ server-side/test.go
@@ -3,19 +3,40 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"fmt"
+
+
 )
 
+type ContactDetails struct {
+	userName   string
+	password string
+}
 
 
 func main() {
-    fs := http.FileServer(http.Dir("../client-side/static"))
+
+
+  fs := http.FileServer(http.Dir("../client-side/static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	tmpl := template.Must(template.ParseFiles("../client-side/static/html/index.html"))
 
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		if r.Method != http.MethodPost {
+			fmt.Println("in if")
+			tmpl.Execute(w, nil)
+			return
+		}
+
+		details := ContactDetails{
+			userName:   r.FormValue("user"),
+			password: r.FormValue("pass"),
+		}
+
+		fmt.Println(details.userName)
+
 	})
 
 	http.ListenAndServe(":8080", nil)
